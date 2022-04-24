d5db9e99893d44648b4ce28c1e8dfec1d089be3c
Miguel Rico <miguelrico@pop-os.localdomain>
2022-04-24 17:01:54 +0100

    fixs

--- program.py
+++ program.py
@@ -1,4 +1,7 @@
 from views import view as v
 
+#Miguel Rico 30008432
+#Programa que funciona como sistema de armazenamento de países, recorrendo somenteo a listas ligadas
+
 if __name__ == "__main__":
     v.main()
--- views/view.py
+++ views/view.py
@@ -1,16 +1,13 @@
 from models import LinkedList as l
 
-def main():
 
+def main():
     lista_ligada = l.LinkedList()
-  
 
     while True:
         comandos = input().lower().split()
 
         lista_ligada = verify_input(comandos, lista_ligada)
-
-
 
 def verify_input(comandos:list, obj:object) -> object:
     if comandos[0] == "rpi":
@@ -32,34 +29,39 @@ def verify_input(comandos:list, obj:object) -> object:
     elif comandos[0] == "rpii":
         obj.insert_at_index(comandos[2], comandos[1])
         obj.traverse_list()
-    
+
     elif comandos[0] == "vne":
         get_count:int = -1 
         get_count = obj.insert_at_index(comandos[2], comandos[1])
-
         print(f"O número de elementos são {get_count}.")
 
     elif comandos[0] == "vp":
-        verify_country:bool = 0 
+        verify_country:bool = 0
         verify_country = obj.search_item(comandos[1])
 
         if verify_country == 1:
             print(f"O país {comandos[1]} encontra-se na lista.")
         else:
-            print(f"O país {comandos[1]}não se encontra na lista.")
+            print(f"O país {comandos[1]} não se encontra na lista.")
 
     elif comandos[0] == "epe":
-        obj.start_node.get_item
+        country:str = obj.start_node.get_item()
         obj.delete_at_start()
-
-        print(f"O país {comandos[1]} foi eliminado da lista.")
+        print(f"O país {country} foi eliminado da lista.")
 
     elif comandos[0] == "eue":
         obj.delete_at_start()
-
         print(f"O país {comandos[1]} foi eliminado da lista.")
 
+    elif comandos[0] == "ep":
+        verify_country_exists: bool = 0
 
-       
+        verify_country_exists = obj.search_item(comandos[1])
+
+        if verify_country_exists == 1:
+            obj.delete_element_by_value(comandos[1])
+            print(f"O país {comandos[1]} foi eliminado da lista.")
+        else:
+            print(f"O país {comandos[1]} não se encontra na lista.")
 
     return obj
